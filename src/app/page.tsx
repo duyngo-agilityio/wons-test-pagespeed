71cbdf1aca3f0f77c8c84dc8755e55dd02a60ce1
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-09-06 01:10:10 +0700

    fix: update style for input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import { Input } from '@/components/common';
 
 const Homepage = () => {
   return (
-    <main className="container mx-auto bg-white p-10">
+    <main className="container mx-auto bg-[#ffffff] p-10">
       <h1 className="text-lg font-semibold">This is homepage</h1>
       <Input
         label="Full Name"
         isInvalid
         errorMessage="Please enter full name"
       />
+      <Input label="Full Name" className="py-10" />
+
+      <Input color="secondary" size="sm" className="py-10" />
     </main>
   );
 };
--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -10,21 +10,41 @@ import {
 const CustomInput = extendVariants(NextUIInput, {
   variants: {
     color: {
-      default: {
-        inputWrapper: `bg-gray-50 dark:bg-gray-600`,
+      primary: {
+        inputWrapper: [
+          'bg-gray-50 dark:bg-gray-600',
+          'hover:!bg-gray-200/50 dark:hover:!bg-gray-900',
+          'focus-within:!bg-gray-50 dark:focus-within:bg-gray-600',
+          'group-data-[focus=true]:!bg-gray-50 dark:group-data-[focus=true]:!bg-gray-600',
+        ],
         input: [
-          `!text-blue-800 dark:!text-white`,
-          `placeholder-blue-800 dark:placeholder-white`,
+          '!text-blue-800/70 dark:!text-white/70',
+          'placeholder-blue-800/20 dark:placeholder-white/20',
+          'bg-transparent',
+        ],
+        label: [
+          'group-data-[filled-within=true]:!text-blue-800 dark:group-data-[filled-within=true]:!text-white',
+        ],
+      },
+      secondary: {
+        inputWrapper: [
+          'bg-gray-200/30 dark:bg-gray-600/30',
+          'hover:!bg-gray-200/50 dark:hover:!bg-gray-600/80',
+          'focus-within:!bg-gray-200/30 dark:focus-within:bg-gray-600/30',
+          'group-data-[focus=true]:!bg-gray-200/30 dark:group-data-[focus=true]:!bg-gray-600/30',
+        ],
+        input: [
+          '!text-blue-800/70 dark:!text-white/70',
+          'placeholder-blue-800/20 dark:placeholder-white/20',
         ],
         label:
           'group-data-[filled-within=true]:text-blue-800 dark:group-data-[filled-within=true]:text-white',
       },
     },
-
     size: {
-      xs: {
-        inputWrapper: 'h-auto px-1',
-        input: 'text-tiny',
+      sm: {
+        inputWrapper: 'h-[42px] px-5 pt-3 pb-3.75',
+        input: 'text-xl',
       },
       md: {
         inputWrapper: 'h-12.5 p-3.75',
@@ -32,28 +52,21 @@ const CustomInput = extendVariants(NextUIInput, {
         base: 'data-[has-label=true]:mt-[29px]',
         label: 'font-medium text-xl top-5',
       },
-      // xl: {
-      //   inputWrapper: 'h-14 min-h-14',
-      //   input: 'text-medium',
-      // },
     },
     radius: {
       sm: {
-        inputWrapper: 'rounded-1.25',
+        inputWrapper: 'rounded-[5px]',
       },
-    },
-    border: {
-      default: {
-        inputWrapper: 'border: 1px solid',
+      md: {
+        inputWrapper: 'rounded-[10px]',
       },
     },
   },
 
   defaultVariants: {
-    color: 'default',
+    color: 'primary',
     size: 'md',
-    radius: 'sm',
-    border: 'default',
+    radius: 'md',
     labelPlacement: 'outside',
     placeholder: ' ',
   },
@@ -62,7 +75,10 @@ const CustomInput = extendVariants(NextUIInput, {
 const Input = ({ ...props }: InputProps): JSX.Element => {
   return (
     <CustomInput
-      // classNames={{ innerWrapper: 'focus:border-red-500' }}
+      classNames={{
+        inputWrapper:
+          'group-data-[focus-visible=true]:ring-2 group-data-[focus-visible=true]:ring-offset-2 group-data-[focus-visible=true]:ring-blue-500',
+      }}
       {...props}
     />
   );
