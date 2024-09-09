7ee62d2c3e659aaa32048a1c0a734264ecd96dfb
kien.nguyen <kien.nguyen@asnet.com.vn>
2024-09-09 18:50:36 +0700

    fix: style

--- src/components/TopSellingProduct/index.tsx
+++ src/components/TopSellingProduct/index.tsx
@@ -3,8 +3,6 @@ import { products } from '@/mocks';
 
 // components
 import { ProductCard } from '@/components';
-
-// layouts
 import { TableLayout } from '@/layouts';
 
 const TopSellingProducts = () => {
@@ -15,17 +13,19 @@ const TopSellingProducts = () => {
           { id, imageUrl, title = 'Product Title', price = 0, rating = 0 },
           index,
         ) => (
-          <div key={id}>
-            <ProductCard
-              url={imageUrl}
-              title={title}
-              price={price}
-              rating={rating}
-              className="shadow-none py-11"
-            />
-            {index < products.length - 1 && (
-              <div className="border-t border-gray-300 dark:border-gray-600 my-4" />
-            )}
+          <div key={id} className="h-[150px] flex items-center">
+            <div className="w-full">
+              <ProductCard
+                url={imageUrl}
+                title={title}
+                price={price}
+                rating={rating}
+                className="shadow-none"
+              />
+              {index < products.length - 1 && (
+                <div className="border-t border-red-300 dark:border-gray-600 mt-4" />
+              )}
+            </div>
           </div>
         ),
       )}
