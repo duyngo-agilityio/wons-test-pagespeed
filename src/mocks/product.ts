e1e0da72ae34f780fd734121a694ad1f825d39a2
Kien Duong Trung Nguyen <kien.nguyen@asnet.com.vn>
2024-09-09 16:39:51 +0700

    Fix/ui top selling

--- src/mocks/product.ts
+++ src/mocks/product.ts
@@ -3,23 +3,18 @@ import { IProduct } from '@/models';
 export const products: IProduct[] = [
   {
     id: 1,
-    imageUrl: 'https://example.com/shoe1.png',
+    imageUrl:
+      'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
     title: 'Web Development',
     price: 87,
     rating: 4,
   },
   {
     id: 2,
-    imageUrl: 'https://example.com/shoe2.png',
+    imageUrl:
+      'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
     title: 'Graphic Design',
     price: 120,
     rating: 5,
-  },
-  {
-    id: 3,
-    imageUrl: 'https://example.com/shoe3.png',
-    title: 'Digital Marketing',
-    price: 99,
-    rating: 3,
   },
 ];
