d1aabf0f9b59520efce7e84952864cd313751f02
kien.nguyen <kien.nguyen@asnet.com.vn>
2024-09-10 09:22:06 +0700

    fix: ui dashboard

--- src/components/TopSellingProduct/index.tsx
+++ src/components/TopSellingProduct/index.tsx
@@ -3,29 +3,29 @@ import { products } from '@/mocks';
 
 // components
 import { ProductCard } from '@/components';
+
+// layouts
 import { TableLayout } from '@/layouts';
 
 const TopSellingProducts = () => {
   return (
-    <TableLayout title="Top Selling Service">
+    <TableLayout title="Top Selling Products" className="h-full md:p-5">
       {products.map(
         (
           { id, imageUrl, title = 'Product Title', price = 0, rating = 0 },
           index,
         ) => (
-          <div key={id} className="h-[150px] flex items-center">
-            <div className="w-full">
-              <ProductCard
-                url={imageUrl}
-                title={title}
-                price={price}
-                rating={rating}
-                className="shadow-none"
-              />
-              {index < products.length - 1 && (
-                <div className="border-t border-red-300 dark:border-gray-600 mt-4" />
-              )}
-            </div>
+          <div key={id} className="mb-6 w-full">
+            <ProductCard
+              url={imageUrl}
+              title={title}
+              price={price}
+              rating={rating}
+              className="shadow-none py-4"
+            />
+            {index < products.length - 1 && (
+              <div className="border-t border-gray-300 dark:border-gray-600 my-8" />
+            )}
           </div>
         ),
       )}
