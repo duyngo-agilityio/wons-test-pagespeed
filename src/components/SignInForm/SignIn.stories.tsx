8adf9388f455466c1a4ebb459518b1a41ec08855
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-09-12 17:08:39 +0700

    Update storybook

--- src/components/SignInForm/SignIn.stories.tsx
+++ src/components/SignInForm/SignIn.stories.tsx
@@ -1,18 +1,18 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import { fn } from '@storybook/test';
 
 // Components
 import SignInForm from './index';
 
 const meta: Meta<typeof SignInForm> = {
+  title: 'Components/SignInForm',
   component: SignInForm,
 };
 
 export default meta;
-type Story = StoryObj<typeof SignInForm>;
 
-export const Default: Story = {
-  args: {
-    onSubmit: fn(),
-  },
-};
+export const Default = () => (
+  <div className="bg-white dark:bg-gray-400 w-[600px] p-[20px] rounded-2xl">
+    <SignInForm onSubmit={fn()} />
+  </div>
+);
