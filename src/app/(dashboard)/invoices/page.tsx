5bc857491181c3e1fcc8ea0dbaa9260db9132268
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-09-13 12:10:20 +0700

    fix: update snapshot

--- src/app/(dashboard)/invoices/page.tsx
+++ src/app/(dashboard)/invoices/page.tsx
@@ -4,17 +4,12 @@ import { DashBoardLayout } from '@/layouts';
 // Sections
 import { InvoiceList, InvoiceListActions } from '@/ui';
 
-const InvoiceListPage = (): JSX.Element => {
-  return (
-    <main>
-      <DashBoardLayout
-        title="Invoice List"
-        rightContent={<InvoiceListActions />}
-      >
-        <InvoiceList />
-      </DashBoardLayout>
-    </main>
-  );
-};
+const InvoiceListPage = (): JSX.Element => (
+  <main>
+    <DashBoardLayout title="Invoice List" rightContent={<InvoiceListActions />}>
+      <InvoiceList />
+    </DashBoardLayout>
+  </main>
+);
 
 export default InvoiceListPage;
