8d933b128f803359d059f1d12333ad2ba246ff2e
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-09-13 08:33:29 +0700

    Feat/handle sort recent services table

--- src/ui/main-dashboard/RecentServicesSection/index.tsx
+++ src/ui/main-dashboard/RecentServicesSection/index.tsx
@@ -10,23 +10,34 @@ import { TableLayout } from '@/layouts';
 import { IProduct, TInvoiceProduct } from '@/models';
 
 // Types
-import { StrapiModel, StrapiResponse } from '@/types';
+import { ISearchParams, StrapiModel, StrapiResponse } from '@/types';
 
 // Components
 import { RecentServicesTable } from '@/components';
 
-const RecentServicesSection = async () => {
+interface IRecentServicesSection {
+  searchParams: ISearchParams;
+}
+
+const RecentServicesSection = async ({
+  searchParams,
+}: IRecentServicesSection) => {
+  const sortBy = searchParams.sortBy;
+  const order = searchParams.order;
+
   const result: StrapiResponse<
     StrapiModel<TInvoiceProduct<StrapiModel<IProduct>>>[]
-  > = (await getInvoiceProducts()) as StrapiResponse<
-    StrapiModel<TInvoiceProduct<StrapiModel<IProduct>>>[]
-  >;
+  > = (await getInvoiceProducts({
+    sort: searchParams.sortBy
+      ? `${sortBy === 'title' ? `product.${sortBy}` : sortBy}:${order}`
+      : '',
+  })) as StrapiResponse<StrapiModel<TInvoiceProduct<StrapiModel<IProduct>>>[]>;
 
   if (!result.data.length) notFound();
 
   return (
     <TableLayout>
-      <RecentServicesTable data={result.data} />
+      <RecentServicesTable data={result.data} order={order} />
     </TableLayout>
   );
 };
