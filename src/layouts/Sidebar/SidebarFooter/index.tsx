1f1287f0df05aa822e3a7b4911b806229f44df26
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-09-17 22:05:00 +0700

    Update confirm modals for signout

--- src/layouts/Sidebar/SidebarFooter/index.tsx
+++ src/layouts/Sidebar/SidebarFooter/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo, useTransition } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import clsx from 'clsx';
@@ -17,7 +17,8 @@ import {
 import { useToast } from '@/hooks';
 
 // Components
-import { Button, Image, IoLogOut, Text } from '@/components';
+import { Button, ConfirmModal, Image, IoLogOut, Text } from '@/components';
+
 const DynamicThemeSwitcher = dynamic(
   () => import('@/components/ThemeSwitcher'),
   {
@@ -34,65 +35,85 @@ const SidebarFooter = ({
   toggle = SIDE_BAR_STATE.OPEN,
   onLogout,
 }: ISidebarFooter) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
-  const [isPending, startTransition] = useTransition();
+  const [isPending, setIsPending] = useState(false);
   const { showToast } = useToast();
 
-  const handleSignOut = () => {
-    startTransition(async () => {
-      // TODO: Update confirm modal later..
-      const res = await onLogout();
+  const handleOpenModal = useCallback(() => setIsModalOpen(true), []);
 
-      if (typeof res === 'string') {
-        return showToast({
-          status: 'error',
-          title: MESSAGE_STATUS.ERROR,
-          description: res,
-        });
-      }
+  const handleCancelDelete = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
 
-      showToast({
-        description: SUCCESS_MESSAGES.SIGN_OUT,
+  const handleSignOut = useCallback(async () => {
+    setIsModalOpen(false);
+    setIsPending(true);
+
+    const res = await onLogout();
+
+    if (typeof res === 'string') {
+      return showToast({
+        status: 'error',
+        title: MESSAGE_STATUS.ERROR,
+        description: res,
       });
+    }
 
-      router.push(ROUTES.SIGN_IN);
+    showToast({
+      description: SUCCESS_MESSAGES.SIGN_OUT,
     });
-  };
+
+    router.push(ROUTES.SIGN_IN);
+
+    setIsPending(false);
+  }, [router, showToast]);
 
   return (
-    <section className="flex flex-col gap-7.5">
-      <DynamicThemeSwitcher />
-      <div
-        className={clsx(
-          'flex',
-          toggle === SIDE_BAR_STATE.OPEN ? 'justify-between' : 'flex-col gap-3',
-        )}
-      >
-        <div className="flex items-center gap-2.5">
-          <Image
-            src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-            alt="admin"
-            width={40}
-            height={40}
-            className="rounded-10"
-          />
-          {toggle === SIDE_BAR_STATE.OPEN && (
-            <div>
-              <Text text="Easin Arafat" className="text-sm" />
-              <Text text="Admin" className="text-xs opacity-50" />
-            </div>
+    <>
+      <section className="flex flex-col gap-7.5">
+        <DynamicThemeSwitcher />
+        <div
+          className={clsx(
+            'flex',
+            toggle === SIDE_BAR_STATE.OPEN
+              ? 'justify-between'
+              : 'flex-col gap-3',
           )}
-        </div>
-        <Button
-          isLoading={isPending}
-          isIconOnly
-          className="!bg-transparent dark:!bg-transparent hover:bg-transparent w-fit h-fit p-2"
-          onClick={handleSignOut}
         >
-          <IoLogOut className="text-blue-800/40 dark:text-white/50 w-5 h-5" />
-        </Button>
-      </div>
-    </section>
+          <div className="flex items-center gap-2.5">
+            <Image
+              src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+              alt="admin"
+              width={40}
+              height={40}
+              className="rounded-10"
+            />
+            {toggle === SIDE_BAR_STATE.OPEN && (
+              <div>
+                <Text text="Easin Arafat" className="text-sm" />
+                <Text text="Admin" className="text-xs opacity-50" />
+              </div>
+            )}
+          </div>
+          <Button
+            isIconOnly
+            isLoading={isPending}
+            className="!bg-transparent dark:!bg-transparent hover:bg-transparent w-fit h-fit p-2"
+            onClick={handleOpenModal}
+          >
+            <IoLogOut className="text-blue-800/40 dark:text-white/50 w-5 h-5" />
+          </Button>
+        </div>
+      </section>
+      <ConfirmModal
+        title="SignOut"
+        content="Are you sure you would like to SignOut?"
+        isOpen={isModalOpen}
+        onConfirm={handleSignOut}
+        onCancel={handleCancelDelete}
+      />
+    </>
   );
 };
 
