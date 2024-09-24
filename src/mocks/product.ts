0f0c19548764090283c6f992481abfd32e0443a6
Kien Duong Trung Nguyen <kien.nguyen@asnet.com.vn>
2024-09-24 13:39:44 +0700

    fix: test handle function add new invoices

--- src/mocks/product.ts
+++ src/mocks/product.ts
@@ -36,3 +36,27 @@ export const mockProducts: StrapiModel<IProduct>[] = [
     },
   },
 ];
+
+export const MOCK_PRODUCTS_WITHOUT_ATTRIBUTES: Omit<IProduct, 'id'>[] = [
+  {
+    title: 'Product 1',
+    price: 100,
+    rating: 5,
+    imageUrl:
+      'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
+  },
+  {
+    title: 'Product 2',
+    price: 200,
+    rating: 4,
+    imageUrl:
+      'https://img-cdn.pixlr.com/image-generator/history/another-product/medium.webp',
+  },
+  {
+    title: 'Product 3',
+    price: 300,
+    rating: 4.5,
+    imageUrl:
+      'https://img-cdn.pixlr.com/image-generator/history/third-product/medium.webp',
+  },
+];
