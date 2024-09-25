a02f5425947b28644a4e78c29383f8539a1754ec
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-09-26 00:14:18 +0700

    [W-138] Write unit test for server actions to edit invoices

--- src/actions/__test__/invoice.test.ts
+++ src/actions/__test__/invoice.test.ts
@@ -11,11 +11,15 @@ import { TInvoiceProductTable } from '@/types';
 import { MOCK_PRODUCTS_WITHOUT_ATTRIBUTES } from '@/mocks';
 
 // actions
-import { createInvoiceProducts } from '@/actions';
+import { createInvoiceProducts, editInvoice } from '@/actions';
+
+// Constants
+import { InvoiceStatus } from '@/constants';
 
 jest.mock('@/services', () => ({
   httpClient: {
     postRequest: jest.fn(),
+    putRequest: jest.fn(),
   },
 }));
 
@@ -83,6 +87,50 @@ describe('createInvoiceProducts', () => {
 
     expect(httpClient.postRequest).toHaveBeenCalled();
 
+    expect(formatErrorMessage).toHaveBeenCalledWith(MOCK_ERROR);
+  });
+});
+
+describe('editInvoice', () => {
+  const newData = { email: 'test@gmai.com' };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should successfully update invoice', async () => {
+    const MOCK_RESPONSE = {
+      id: 4,
+      attributes: {
+        email: 'test@gmai.com',
+        date: '2024-09-10',
+        status: InvoiceStatus.COMPLETE,
+        address: '1254 Xo Viet Nghe Tinh, Da Nang',
+        isSelected: true,
+        invoiceId: '871345',
+        imageUrl: '',
+      },
+    };
+
+    (httpClient.putRequest as jest.Mock).mockResolvedValue(MOCK_RESPONSE);
+
+    await editInvoice(1, newData, []);
+
+    expect(httpClient.putRequest).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return an error with formatted message when editInvoice fails', async () => {
+    const MOCK_ERROR = new Error('Request failed');
+    const FORMATTED_ERROR_MESSAGE = 'Something went wrong.';
+
+    (httpClient.putRequest as jest.Mock).mockRejectedValue(MOCK_ERROR);
+
+    (formatErrorMessage as jest.Mock).mockReturnValue(FORMATTED_ERROR_MESSAGE);
+
+    const result = await editInvoice(1, newData, []);
+
+    expect(result).toEqual({ error: FORMATTED_ERROR_MESSAGE });
+    expect(httpClient.putRequest).toHaveBeenCalled();
     expect(formatErrorMessage).toHaveBeenCalledWith(MOCK_ERROR);
   });
 });
