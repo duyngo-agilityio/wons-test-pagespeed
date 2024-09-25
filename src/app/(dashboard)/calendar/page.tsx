b365a1c6489d2579818ad2e2d7f4eec3bf80afd0
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-09-25 11:49:12 +0700

    It should show “Coming soon” instead of Not Found for pages that have not been implemented.

---
+++ src/app/(dashboard)/calendar/page.tsx
@@ -0,0 +1,12 @@
+import { Heading, Text } from '@/components';
+
+const CalendarPage = () => {
+  return (
+    <div>
+      <Heading size="lg" title="Calendar page" />
+      <Text text="Coming soon!" className="mt-2" />
+    </div>
+  );
+};
+
+export default CalendarPage;
---
+++ src/app/(dashboard)/products/page.tsx
@@ -0,0 +1,12 @@
+import { Heading, Text } from '@/components';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Heading size="lg" title="Product page" />
+      <Text text="Coming soon!" className="mt-2" />
+    </div>
+  );
+};
+
+export default ProductPage;
