a1c7b1015410e097413330c3bfeb5d8ac69c26ba
tantran-agilityio <tan.tran@asnet.com.vn>
2024-10-02 14:35:07 +0700

    fix: disable coming soon tabs

--- src/mocks/tabs.ts
+++ src/mocks/tabs.ts
@@ -1,11 +1,22 @@
 export const EVENT_TABS = [
-  { key: 'event', label: 'Event', content: 'This is the event content' },
+  {
+    key: 'event',
+    label: 'Event',
+    content: 'This is the event content',
+    isDisable: false,
+  },
   {
     key: 'reminder',
     label: 'Reminder',
     content: 'This is the reminder content',
+    isDisable: true,
   },
-  { key: 'task', label: 'Task', content: 'This is the task content' },
+  {
+    key: 'task',
+    label: 'Task',
+    content: 'This is the task content',
+    isDisable: true,
+  },
 ];
 
 export const TASK_TABS = [
