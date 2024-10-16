7ff6c95d79d84959a85eb7ae14c92e91fa5bdaa0
duy.ngo <duy.ngo@asnet.com.vn>
2024-10-16 15:48:46 +0700

    feat: write unit test service api for update delete event

--- src/actions/__test__/events.test.ts
+++ src/actions/__test__/events.test.ts
@@ -1,7 +1,7 @@
 import { revalidateTag } from 'next/cache';
 
 // api
-import { createEvent } from '../event';
+import { createEvent, deleteEvent, updateEvent } from '../event';
 
 // services
 import { httpClient } from '@/services';
@@ -11,11 +11,15 @@ import { API_PATH } from '@/constants';
 
 // utils
 import { formatErrorMessage } from '@/utils';
+
+// Mocks
 import { EVENT_MOCKS } from '@/mocks';
 
 jest.mock('@/services', () => ({
   httpClient: {
     postRequest: jest.fn(),
+    deleteRequest: jest.fn(),
+    putRequest: jest.fn(),
   },
 }));
 
@@ -65,6 +69,67 @@ describe('createEvent', () => {
     (formatErrorMessage as jest.Mock).mockReturnValue('Something went wrong.');
 
     const result = await createEvent(EVENT_MOCKS);
+
+    expect(formatErrorMessage).toHaveBeenCalledWith(MOCK_ERROR);
+    expect(result).toEqual({ error: 'Something went wrong.' });
+  });
+});
+
+describe('deleteEvent', () => {
+  const eventID = 7;
+
+  it('calls success', async () => {
+    (httpClient.deleteRequest as jest.Mock).mockResolvedValue({
+      endpoint: `${API_PATH.EVENTS}/${eventID}`,
+    });
+
+    await deleteEvent(eventID);
+
+    expect(httpClient.deleteRequest).toHaveBeenCalledWith({
+      endpoint: `${API_PATH.EVENTS}/${eventID}`,
+    });
+    expect(revalidateTag).toHaveBeenCalledWith(API_PATH.EVENTS);
+  });
+
+  it('calls failed', async () => {
+    const MOCK_ERROR = new Error('Request failed');
+    (httpClient.deleteRequest as jest.Mock).mockRejectedValue(MOCK_ERROR);
+    (formatErrorMessage as jest.Mock).mockReturnValue('Something went wrong.');
+
+    const result = await deleteEvent(eventID);
+
+    expect(revalidateTag).not.toHaveBeenCalled();
+    expect(result).toEqual({ error: 'Something went wrong.' });
+  });
+});
+
+describe('updateEvent', () => {
+  const eventID = 1;
+
+  it('calls success', async () => {
+    (httpClient.putRequest as jest.Mock).mockResolvedValue({
+      data: {
+        id: eventID,
+        ...EVENT_MOCKS,
+      },
+    });
+
+    await updateEvent(eventID, EVENT_MOCKS);
+
+    expect(httpClient.putRequest).toHaveBeenCalledWith({
+      endpoint: `${API_PATH.EVENTS}/${eventID}`,
+      body: { data: EVENT_MOCKS },
+    });
+
+    expect(revalidateTag).toHaveBeenCalledWith(API_PATH.EVENTS);
+  });
+
+  it('calls failed', async () => {
+    const MOCK_ERROR = new Error('Update failed');
+    (httpClient.putRequest as jest.Mock).mockRejectedValue(MOCK_ERROR);
+    (formatErrorMessage as jest.Mock).mockReturnValue('Something went wrong.');
+
+    const result = await updateEvent(eventID, EVENT_MOCKS);
 
     expect(formatErrorMessage).toHaveBeenCalledWith(MOCK_ERROR);
     expect(result).toEqual({ error: 'Something went wrong.' });
