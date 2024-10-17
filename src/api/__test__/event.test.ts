01371e9dec910148987d15523ae0bf657fe714a7
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-10-17 16:20:29 +0700

    [W-246] Write unit test for API to get event list

---
+++ src/api/__test__/event.test.ts
@@ -0,0 +1,59 @@
+// Services
+import { httpClient } from '@/services';
+
+// Mocks
+import { EVENTS_MOCKS } from '@/mocks';
+
+// Constants
+import { ERROR_MESSAGES } from '@/constants';
+
+// Utils
+import { formatErrorMessage } from '@/utils';
+
+// Api
+import { getEvents } from '../event';
+
+jest.mock('@/utils', () => ({
+  formatErrorMessage: jest.fn(),
+}));
+
+jest.mock('next/cache', () => ({
+  revalidateTag: jest.fn(),
+}));
+
+jest.mock('@/services', () => ({
+  httpClient: {
+    getRequest: jest.fn(),
+  },
+}));
+
+describe('Events', () => {
+  it('should get the event list successfully', async () => {
+    const mocksEvents = EVENTS_MOCKS;
+
+    jest
+      .spyOn(httpClient, 'getRequest')
+      .mockResolvedValue({ data: mocksEvents });
+
+    const response = await getEvents();
+
+    expect(response).toEqual({ data: mocksEvents });
+  });
+
+  it('should get the event list failed', async () => {
+    const MOCK_ERROR = new Error('Update failed');
+    (httpClient.getRequest as jest.Mock).mockRejectedValue({
+      error: MOCK_ERROR,
+    });
+    (formatErrorMessage as jest.Mock).mockReturnValue(
+      ERROR_MESSAGES.UNKNOWN_ERROR,
+    );
+
+    const result = await getEvents();
+
+    expect(formatErrorMessage).toHaveBeenCalledWith({
+      error: MOCK_ERROR,
+    });
+    expect(result).toEqual({ error: ERROR_MESSAGES.UNKNOWN_ERROR });
+  });
+});
--- src/mocks/event.ts
+++ src/mocks/event.ts
@@ -7,3 +7,18 @@ export const EVENT_MOCKS = {
   endTime: '12:00',
   users_permissions_users: [1, 2],
 };
+
+export const EVENTS_MOCKS = [
+  {
+    id: 1,
+    attributes: {
+      title: 'Test Event',
+      status: 'active',
+      location: 'New York',
+      date: new Date('2024-10-14'),
+      startTime: '10:00',
+      endTime: '12:00',
+      users_permissions_users: [1, 2],
+    },
+  },
+];
