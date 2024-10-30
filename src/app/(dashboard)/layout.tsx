12387705749110c7fc4b7252bb984da5ce871550
duy.ngo <duy.ngo@asnet.com.vn>
2024-10-30 15:24:37 +0700

    fix: add suspense sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 // Layouts
 import { Sidebar } from '@/layouts';
+import { Suspense } from 'react';
 
 const RootLayout = ({
   children,
@@ -7,7 +8,9 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => (
   <div className="lg:flex">
-    <Sidebar />
+    <Suspense>
+      <Sidebar />
+    </Suspense>
     <div className="flex-1 pt-7.5 base:px-6 md:pl-7.5 md:pr-7 mb-8 max-w-[1324px] mx-auto">
       {children}
     </div>
