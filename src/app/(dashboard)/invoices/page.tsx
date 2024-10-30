95d47eadec55a552cd4c0eb39a77c7cd6d3f299c
duy.ngo <duy.ngo@asnet.com.vn>
2024-10-30 15:38:00 +0700

    fix: add layout dashboard again

--- src/app/(dashboard)/invoices/page.tsx
+++ src/app/(dashboard)/invoices/page.tsx
@@ -10,11 +10,10 @@ import { IMAGES } from '@/constants/images';
 import { ISearchParams } from '@/types/params';
 
 // Layouts
-// import { DashBoardLayout } from '@/layouts';
+import { DashBoardLayout } from '@/layouts';
 
 // Sections
 import { InvoiceListActions, InvoiceList, InvoiceListSkeleton } from '@/ui';
-import { Heading } from '@/components';
 
 export const metadata: Metadata = {
   title: 'Wons Invoice',
@@ -49,32 +48,22 @@ const InvoiceListPage = ({
 
   return (
     <main>
-      {/* <DashBoardLayout
+      <DashBoardLayout
         title={PAGE_TITLES.INVOICE}
         rightContent={<InvoiceListActions />}
-      > */}
-      <div
-        className={
-          'base:mb-6 md:mb-7.5 w-full md:flex justify-between md:items-center'
-        }
       >
-        <Heading className="w-full" title={PAGE_TITLES.INVOICE} />
-        <Suspense>
-          <InvoiceListActions />
+        <Suspense
+          key={order + sortBy + query + page}
+          fallback={<InvoiceListSkeleton />}
+        >
+          <InvoiceList
+            sortBy={sortBy}
+            sortOrder={order}
+            query={query}
+            page={+page}
+          />
         </Suspense>
-      </div>
-      <Suspense
-        key={order + sortBy + query + page}
-        fallback={<InvoiceListSkeleton />}
-      >
-        <InvoiceList
-          sortBy={sortBy}
-          sortOrder={order}
-          query={query}
-          page={+page}
-        />
-      </Suspense>
-      {/* </DashBoardLayout> */}
+      </DashBoardLayout>
     </main>
   );
 };
