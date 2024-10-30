72ab5e59ef3b2ab21d2c9db4bf7e05c0a85ebae2
duy.ngo <duy.ngo@asnet.com.vn>
2024-10-30 14:43:55 +0700

    feat: remove aria level

--- src/components/common/Heading/index.tsx
+++ src/components/common/Heading/index.tsx
@@ -17,21 +17,10 @@ const Heading = ({
   as: Component = 'h2',
   className = '',
 }: HeadingProps) => {
-  const ariaLevels: { [key: string]: number } = {
-    h1: 1,
-    h2: 2,
-    h3: 3,
-    h4: 4,
-    h5: 5,
-    h6: 6,
-  };
-
-  const ariaLevel = ariaLevels[Component];
   const fontSizeClass = HEADING_SIZE_MAP[size];
 
   return (
     <Component
-      aria-level={ariaLevel}
       className={`font-bold font-dm-sans text-blue-800 dark:text-white ${fontSizeClass} ${className}`}
     >
       {title}
