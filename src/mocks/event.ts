f52b054dd7a372662268a745c1c5a79af8de5b7d
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-10-31 17:13:53 +0700

    Improve unit test utils

--- src/mocks/event.ts
+++ src/mocks/event.ts
@@ -35,3 +35,31 @@ export const EVENTS_MOCKS = [
     },
   },
 ];
+
+export const EVENTS_MOCKS_WIDTH_USERS_PERMISSIONS = {
+  title: 'Test Event',
+  status: 'active',
+  location: 'New York',
+  date: new Date('2024-10-14'),
+  startTime: '10:00',
+  endTime: '12:00',
+  users_permissions_users: {
+    data: [
+      {
+        id: 3,
+        attributes: {
+          username: 'superadmin',
+          email: 'admin1@gmail.com',
+          provider: 'local',
+          confirmed: false,
+          blocked: false,
+          fullName: 'Super Admin',
+          createdAt: '2024-08-28T04:29:52.000Z',
+          updatedAt: '2024-10-11T02:14:38.208Z',
+          avatar:
+            'https://watermark.lovepik.com/photo/20211209/large/lovepik-japanese-fresh-girl-park-photo-picture_501698500.jpg',
+        },
+      },
+    ],
+  },
+};
